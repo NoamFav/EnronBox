4a225f0190da8c9a83ab5ab2f1895b2b80072db3
DuckBlood_J <160181383+DuckBloodJ@users.noreply.github.com>
2025-03-28 22:17:59 +0100

    Updated NER method

--- src/ner/extractor.py
+++ src/ner/extractor.py
@@ -2,23 +2,24 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-def extract_entities(body: str) -> dict:
-    email = nlp(body)
+class Extractor:
+    def extract_entities(self, body: str) -> dict:
+        email = nlp(body)
 
-    names = []
-    orgs = []
-    dates = []
+        names = []
+        orgs = []
+        dates = []
 
-    for ent in email.ents:
-        if ent.label_ == "PERSON":
-            names.append(ent.text)
-        elif ent.label_ == "ORG":
-            orgs.append(ent.text)
-        elif ent.label_ == "DATE":
-            dates.append(ent.text)
+        for ent in email.ents:
+            if ent.label_ == "PERSON":
+                names.append(ent.text)
+            elif ent.label_ == "ORG":
+                orgs.append(ent.text)
+            elif ent.label_ == "DATE":
+                dates.append(ent.text)
 
-    return {
-        "names": names,
-        "orgs": orgs,
-        "dates": dates
-    }
+        return {
+            "names": names,
+            "orgs": orgs,
+            "dates": dates
+        }
