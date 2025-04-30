9002fdfaf9b2300601095927ab2c5ecd418c6a2f
noamfav <95364866+NoamFav@users.noreply.github.com>
2025-04-30 09:57:23 +0200

    chore(dependencies): update package dependencies to latest versions for security

---
+++ test.py
@@ -0,0 +1,12 @@
+import ollama
+
+response = ollama.chat(
+    model="mistral",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is the sky blue?",
+        },
+    ],
+)
+print(response["message"]["content"])
