7d10968fde00b9a0bcd6e83c026b98a3d6c0d1b0
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-05-27 02:03:09 +0200

    feat (tauri): launch flask-backend on application startup

--- apps/enron_classifier/src-tauri/src/lib.rs
+++ apps/enron_classifier/src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::process::{Command, Stdio};
+use tauri::path::BaseDirectory;
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -9,6 +12,20 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let resource_path = app
+                .path()
+                .resolve("bin/flask-backend", BaseDirectory::Resource)
+                .expect("Failed to resolve resource path");
+
+            Command::new(resource_path)
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .spawn()
+                .expect("Failed to launch Flask backend");
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
