37522482c02546eaed4759ca5f9267f98c531005
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-06-02 22:41:25 +0200

    Update lib.rs

--- apps/enron_classifier/src-tauri/src/lib.rs
+++ apps/enron_classifier/src-tauri/src/lib.rs
@@ -12,20 +12,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
-            let resource_path = app
-                .path()
-                .resolve("bin/flask-backend", BaseDirectory::Resource)
-                .expect("Failed to resolve resource path");
-
-            Command::new(resource_path)
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .spawn()
-                .expect("Failed to launch Flask backend");
-
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
