8581ef62132429184d53c6a433fc1739143b9784
DuckBlood_J <160181383+DuckBloodJ@users.noreply.github.com>
2025-06-03 21:06:30 +0200

    Email Flag

--- apps/flask_api/app/main.py
+++ apps/flask_api/app/main.py
@@ -1,10 +1,17 @@
 from app.ui.enron_shell import EnronMailShell
-
+from app.routes.emails import emails_bp
+from flask import request, jsonify, Flask
+from flask_cors import CORS
+from app.services import db
 import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(emails_bp, url_prefix="/api")
 
 def main():
     maildir_path = "./maildir"
@@ -36,6 +43,19 @@ def main():
     shell = EnronMailShell(maildir_path, max_emails)
     shell.run()
 
+@app.route('/api/emails/<email_id>/status', methods=['POST', 'OPTIONS'])
+def update_email_status(email_id):
+
+    if request.method == 'OPTIONS':
+         return '', 200
+    
+    status_update = request.json
+    print(f"Received status update for email {email_id}:", status_update)
+    
+    success = db.store_email_status(email_id, status_update)
+    if success:
+        return jsonify({"message": "Email status updated successfully"}), 200
+    return jsonify({"message": "Failed to update email status"}), 400
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=5050, debug=True)
